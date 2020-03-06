d2b5c15de8a5895aac829a713a59615921529a12
Kelsey Dickson <kelseymedved@gmail.com>
2020-03-06 14:22:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,18 +16,30 @@ function nowServing(katzDeliLine){
 }
 
 
-function currentLine(katzDeliLine){
-let results = '';
-  if(katzDeliLine.length === 0){
-      return `The line is currently empty.`;
-    } else {
-      for(let i = 0; i <katzDeliLine.length; i++){
-        if(i === katzDeliLine.length - 1){
-          results += `${i+1}. ${katzDeliLine[i]}`;
-        } else {
-      results += `${[i+1]}. ${katzDeliLine[i]}, `;
+// function currentLine(katzDeliLine){
+// let results = '';
+//   if(katzDeliLine.length === 0){
+//       return `The line is currently empty.`;
+//     } else {
+//       for(let i = 0; i <katzDeliLine.length; i++){
+//         if(i === katzDeliLine.length - 1){
+//           results += `${i+1}. ${katzDeliLine[i]}`;
+//         } else {
+//       results += `${[i+1]}. ${katzDeliLine[i]}, `;
+//     }
+//   }
+//             return `The line is currently: ${results}`;
+//     }
+//   }
+
+  function currentLine(katzDeliLine){
+  let results = `The line is currently: 1. ${katzDeliLine[0]}`
+    if(katzDeliLine.length === 0){
+        return `The line is currently empty.`;
+      } else {
+        for(let i = 0; i <katzDeliLine.length; i++){
+          
     }
-  }
-            return `The line is currently: ${results}`;
+              return  results;
+      }
     }
-  }
